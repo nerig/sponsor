84b5a8b8f4fb1523da94b4b2f1a72503a39398f7
nerig <tempaner@gmail.com>
2014-10-29 11:45:57 -0400

    adding back support to homepage and #events

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,10 +2,7 @@
 
 spons.controller('EntireHomepageCtrl', ["$scope", function($scope) {
 	
-	$scope.showInitialHomepage = true;
-	get('homepage-search').focus();
-
-	$scope.onTextChange = function() {
+	var makeEventsVisible = function() {
 		$scope.showInitialHomepage = false;
 		
 		// make events list visible
@@ -13,9 +10,32 @@ spons.controller('EntireHomepageCtrl', ["$scope", function($scope) {
 		listElement.style.opacity = 1;
 		listElement.style.height = "auto";
 		listElement.style.overflowY = "visible";
+	}
+
+	$scope.showInitialHomepage = true;
+	get('homepage-search').focus();
+
+	if (window.location.hash.length > 0) {
+		if (window.location.hash === '#events') {
+			makeEventsVisible();
+		}
+	}
+	
+	$scope.onTextChange = function() {
+		window.location.hash = 'events';
+
+		makeEventsVisible();
 
 		get('free-text-search').focus();
 	}
+
+	window.onhashchange = function() {		
+		// supporting back from any page or hash to the homepage
+		if (window.location.hash === '') {
+			window.location.reload();
+		}
+	}
+
 }])
 
 .controller('HomepageCtrl', ["$scope", function($scope) {
