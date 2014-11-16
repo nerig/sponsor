13f342dbdd44f115e54ff8b96190536eeb60be87
nerig <tempaner@gmail.com>
2014-11-16 18:34:58 -0500

    fixing some colors and shapes in new event form

--- app/assets/javascripts/new_event_form_ctrl.js
+++ app/assets/javascripts/new_event_form_ctrl.js
@@ -18,9 +18,12 @@ spons.controller('newEventFormCtrl', ["$scope", function($scope) {
 		if (sheet.insertRule) {
 			sheet.insertRule('form .ng-untouched.ng-invalid, form .ng-dirty.ng-invalid, form .ng-touched.ng-invalid { border-color: red; box-shadow: none; }', 
 				sheet.cssRules.length);
+			sheet.insertRule('form .ng-untouched.ng-invalid:focus, form .ng-dirty.ng-invalid:focus, form .ng-touched.ng-invalid:focus { outline-offset: 0; outline: -webkit-focus-ring-color auto 0px; }', 
+				sheet.cssRules.length);
 		} else {
 			if (sheet.addRule) {
 				sheet.addRule('form .ng-untouched.ng-invalid, form .ng-dirty.ng-invalid, form .ng-touched.ng-invalid', 'border-color: red; box-shadow: none;', -1);
+				sheet.addRule('form .ng-untouched.ng-invalid:focus, form .ng-dirty.ng-invalid:focus, form .ng-touched.ng-invalid:focus', 'outline-offset: 0; outline: -webkit-focus-ring-color auto 0px;', -1);
 			}
 		}
 	}
