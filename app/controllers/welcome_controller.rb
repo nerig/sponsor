ce6dd15be044ebe269f80190cf672137b03062eb
nerig <tempaner@gmail.com>
2015-01-05 18:23:27 -0500

    removing temp code for updating current events with new escaping methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 		
-		#temp code to update events descriptions in the db
-		Event.all.each { |e| 
-			e.update(description: e.description.gsub("\r\n", "<br />").gsub('"', '&quot;').gsub("'", "&#39;"))
-		}
-
-
 		@events = Event.where("date_time_starts > ?", Time.now).sort { |a, b| a.date_time_starts <=> b.date_time_starts}
 		@six_events = @events[0..5].map { |e|
 			Marshal.load(Marshal.dump(e))
