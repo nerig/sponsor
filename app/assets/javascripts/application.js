254c64454ebf5e0c144668f672fab675d67e7874
nerig <tempaner@gmail.com>
2015-01-18 21:29:39 -0500

    adding history support for create event page
    adding history support for create event page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,3 +61,29 @@ var addValidationsCssRule = function() {
 var go = function(path) {
 	window.location.href = path;
 }
+
+// saves a cookie for 10 days
+var setCookie = function(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+var getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return undefined;
+}
+
+var deleteCookie = function(cname) {
+	var d = new Date();
+    d.setTime(d.getTime() - (24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=; " + expires;
+}
