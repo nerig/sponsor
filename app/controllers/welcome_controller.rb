6df67c89d97f01a59e1464bf4f0f4b1c32f78700
nerig <tempaner@gmail.com>
2015-01-29 14:16:13 -0500

    removing temp code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 		
-		# temp code to decapitalize attendees_income_levels in the already saved events
-		Event.all.each { |e|
-			levels = []
-			e.attendees_income_levels.each { |il| levels << il.downcase }
-			e.update(attendees_income_levels: levels)
-		}
-
 		@events = Event.where("date_time_starts > ?", Time.now).sort { |a, b| a.date_time_starts <=> b.date_time_starts}
 		@six_events = @events[0..5].map { |e|
 			Marshal.load(Marshal.dump(e))
