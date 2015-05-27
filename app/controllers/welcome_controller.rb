3e3d3f00c301b38a0701a866cd935ca66adc1000
nerig <tempaner@gmail.com>
2015-05-27 17:33:52 -0400

    removing update code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,5 @@
 class WelcomeController < ApplicationController
 	def index
-
-		Event.all.each { |e|
-			e.update(user_id: 6)
-		}
 		
 		@events = Event.where("date_time_starts > ?", Time.now).sort { |a, b| a.date_time_starts <=> b.date_time_starts}
 		@six_events = @events[0..5].map { |e|
